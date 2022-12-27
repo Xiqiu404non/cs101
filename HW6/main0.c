43de2bf50272486e38bf55b956ee8333a90c8f8a
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2022-12-28 01:33:16 +0800

    Create main0.c

---
+++ HW6/main0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=10;
+    if (i==1)
+        printf("1 is true");
+  
+    if (i%2==0&&(i/2)%2==0)
+        printf("%d is true", i);
+  
+    else
+        printf("%d is false", i);
+    
+    return 0;
+}
