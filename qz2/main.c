db2bc2f09161b43bc4689b8717599b509d9f0f9d
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2022-12-29 16:42:41 +0800

    Update main.c

--- qz2/main.c
+++ qz2/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-void print_spaces(int r,int rows){
-     printf("%*c" , (rows - r) / 2 + 1, 0 );
-}
-void print_stars(int r){
-    printf("*", r);
+void print_spaces(int r, int rows){
+    int i;
+    for(i = 1;i<=(rows-r)*2;i++){
+        printf(" ");
+    }
+    
 }
 
-int main()
-{   
-    int row=5;
-    row=row*2-1;
-    for(int i = row; i >0; i-=2) {
-        print_spaces(i,row);
-        for(int j = 0; j < i; ++j)
-            print_stars(i);
-        printf("\n");
+void print_stars(int r){
+    int j;
+    for(j = 1;j<=r*(2)-1;j++){
+        printf("* ");
+    }
+    printf("\n");
+}
+
+int main() {
+    int i;
+    int rows = 5;
+    for(int i = rows;i>=1;--i){
+        print_spaces(i, rows);
+        print_stars(i);
     }
     return 0;
 }
