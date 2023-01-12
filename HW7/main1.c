09fffe6853e41cc702ecf670ebe8a35cc295642a
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-01-12 10:43:27 +0800

    Update main1.c

--- HW7/main1.c
+++ HW7/main1.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-        int n = 7, i = 0, j = 0;
-        char c = ' ';
-        for(i =  1; i <= n ; i ++){
+        int n=7, i=0, j=0;
+        char c=' ';
+        for(i=1; i<=n; i++){
             printf("%*c", n-i+1, ' ');
-            for(j = 1; j <= i ; j++){
+            for(j=1; j<=i ;j++){
                 printf("%d ", i);
             }
             printf("\n");
